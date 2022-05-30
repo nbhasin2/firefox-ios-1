e89988bf44e8e8fff2d8f6e179729e9335875887
Nishant Bhasin <nish.bhasin@gmail.com>
2022-05-30 16:25:51 -0400

    Bugfix FXIOS-4285: Replace SDWebImage with Kingfisher to stop Firefox from crashing when opening some PDF which causes memory pressure

--- Client/Application/AppDelegate.swift
+++ Client/Application/AppDelegate.swift
@@ -478,10 +478,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Set the UA for WKWebView (via defaults), the favicon fetcher, and the image loader.
         // This only needs to be done once per runtime. Note that we use defaults here that are
         // readable from extensions, so they can just use the cached identifier.
-
-        SDWebImageDownloader.shared.setValue(firefoxUA, forHTTPHeaderField: "User-Agent")
-        // SDWebImage is setting accept headers that report we support webp. We don't
-        SDWebImageDownloader.shared.setValue("image/*;q=0.8", forHTTPHeaderField: "Accept")
+//        SDWebImageDownloader.shared.setValue(firefoxUA, forHTTPHeaderField: "User-Agent")
+//        // SDWebImage is setting accept headers that report we support webp. We don't
+//        SDWebImageDownloader.shared.setValue("image/*;q=0.8", forHTTPHeaderField: "Accept")
 
         // Record the user agent for use by search suggestion clients.
         SearchViewController.userAgent = firefoxUA
@@ -581,6 +580,16 @@ extension AppDelegate: UINavigationControllerDelegate {
         default:
             return nil
         }
+    }
+    
+    func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
+        print("WARNING!!!!")
+//        SDImageCache.shared.clear(with: .all) {
+//            print("Disk & memory data cleared")
+//        }
+        // Clear image cache
+//        SDImageCache.shared.clearDisk()
+//        SDImageCache.shared.clearMemory()
     }
 }
 
